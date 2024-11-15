59d09dacbb17aea5bdca0fbc1cf1e221f2962517
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-11-15 09:47:34 +0100

    Add files via upload

---
+++ es8/src/persona/Spettacolo.java
@@ -0,0 +1,59 @@
+package persona;
+
+public class Spettacolo {
+    private String titoloSpettacolo;
+    private String nomeCompagnia;
+    private String data;
+    private String ora;
+    private double prezzo;
+
+    private final int posti;
+    private Persona [] spettatori;
+    private int postiOccupati;
+
+    public Spettacolo(String titoloSpettacolo, String nomeCompagnia,String data,String ora,double prezzo,int posti) {
+        this.titoloSpettacolo=titoloSpettacolo;
+        this.nomeCompagnia=nomeCompagnia;
+        this.data=data;
+        this.ora=ora;
+        this.prezzo=prezzo;
+        this.posti = posti;
+        this.postiOccupati=0;
+        this.spettatori=new Persona[posti];
+    }
+
+    public boolean aggiuntaSpettatore(Persona persona){
+        if( postiOccupati < posti ){
+            spettatori[postiOccupati]= persona;
+            postiOccupati++;
+            return true;
+        }
+        return false;
+    }
+
+    public String elencoPrenotazioni(){
+        if( postiOccupati == 0 ){
+            return "NESSUNA PRENOTAZIONE";
+        } else {
+            String s="PRENOTAZIONI ATTUALI PER "+this.titoloSpettacolo+" di "+this.nomeCompagnia+": \n";
+            for (int i=0;i<postiOccupati;i++){
+                s=s+this.spettatori[i].toString()+" Data e ora: "+this.data+" - "+this.ora+" Costo: "+getCostoBiglietto(this.spettatori[i])+"\n";
+            }
+            return s;
+        }
+    }
+
+    public int getPostiLiberi() { return this.posti-this.postiOccupati; }
+
+    public double getCostoBiglietto(Persona persona) {
+        if (persona instanceof Studente || persona instanceof Professore)
+            return (this.prezzo-(0.1*this.prezzo));
+        else
+            return this.prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "SPETTACOLO: "+this.titoloSpettacolo+" di "+this.nomeCompagnia+": \n"+ " Data e ora: "+this.data+" - "+this.ora;
+    }
+}
---
+++ es8/src/persona/Test.java
@@ -0,0 +1,26 @@
+package persona;
+
+public class Test {
+    public static void main(String[] args) {
+        Spettacolo spettacolo=new Spettacolo("la locandiera","pegadorCompany","25/12/2007","11:05",5.00,100);
+        Persona p1=new Persona("Giovanni Rana","MI, via ciao 22");
+        if (spettacolo.aggiuntaSpettatore(p1))
+            System.out.println("INSERITO CORRETTAMENTE");
+        else
+            System.out.println("POSTI TUTTI OCCUPATI");
+        Studente p2=new Studente("Longo Alessandro","Solza, via trota 23");
+        if (spettacolo.aggiuntaSpettatore(p2))
+            System.out.println("INSERITO CORRETTAMENTE");
+        else
+            System.out.println("POSTI TUTTI OCCUPATI");
+        Professore p3=new Professore("Vizz Luca","VCO, via erminia 34","ABX009","Italiano");
+        if (spettacolo.aggiuntaSpettatore(p3))
+            System.out.println("INSERITO CORRETTAMENTE");
+        else
+            System.out.println("POSTI TUTTI OCCUPATI");
+
+        System.out.println(spettacolo); // sono stampate le informazioni relative allo spettacolo: titolo, compagnia data e ora spettacolo
+        System.out.println(spettacolo.elencoPrenotazioni()); //è stampato l’elenco di tutti gli spettatori con il relativo prezzo del biglietto
+        System.out.println("POSTI LIBERI:"+spettacolo.getPostiLiberi());
+    }
+}
