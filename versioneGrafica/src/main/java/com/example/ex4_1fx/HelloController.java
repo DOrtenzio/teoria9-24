efb54b8936f378794ef6ccd44aeabbb68c96c704
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-11-17 21:49:47 +0100

    Errori e lavori grafici
    Correzione di errori grafici ed implementazione di una pima miglioria sempre a livello grafico

--- versioneGrafica/src/main/java/com/example/ex4_1fx/HelloController.java
+++ versioneGrafica/src/main/java/com/example/ex4_1fx/HelloController.java
@@ -2,11 +2,14 @@ package com.example.ex4_1fx;
 
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
@@ -33,23 +36,38 @@ public class HelloController {
         Label l1=new Label("Inserire titolo della rappresentazione:");
         TextField t1=new TextField();
         t1.setPromptText("La locandiera");
+        t1.setStyle("-fx-background-color: #f5ebe0; -fx-background-radius: 4; -fx-textFill: #edede9; -fx-border-color: #999388; -fx-border-width: 1px; -fx-border-radius: 16;");
         Label l2=new Label("Inserire il nome della compagnia teatrale:");
         TextField t2=new TextField();
         t2.setPromptText("PegadorCompany");
+        t2.setStyle("-fx-background-color: #f5ebe0; -fx-background-radius: 16; -fx-textFill: #edede9; -fx-border-color: #999388; -fx-border-width: 1px; -fx-border-radius: 16;");
         Label l3=new Label("Inserire la data in cui avverrà:");
         TextField t3=new TextField();
         t3.setPromptText("12/24/2023");
+        t3.setStyle("-fx-background-color: #f5ebe0; -fx-background-radius: 16; -fx-textFill: #edede9; -fx-border-color: #999388; -fx-border-width: 1px; -fx-border-radius: 16;");
         Label l4=new Label("Inserire l'ora d'inizio:");
         TextField t4=new TextField();
         t4.setPromptText("11:23");
+        t4.setStyle("-fx-background-color: #f5ebe0; -fx-background-radius: 16; -fx-textFill: #edede9; -fx-border-color: #999388; -fx-border-width: 1px; -fx-border-radius: 16;");
         Label l5=new Label("Inserire il prezzo del biglietto intero:");
         TextField t5=new TextField();
         t5.setPromptText("5.00 €");
+        t5.setStyle("-fx-background-color: #f5ebe0; -fx-background-radius: 16; -fx-textFill: #edede9; -fx-border-color: #999388; -fx-border-width: 1px; -fx-border-radius: 16;");
         Label l6=new Label("Inserire il numero di posti:");
         TextField t6=new TextField();
         t6.setPromptText("100");
+        t6.setStyle("-fx-background-color: #f5ebe0; -fx-background-radius: 16; -fx-textFill: #edede9; -fx-border-color: #999388; -fx-border-width: 1px; -fx-border-radius: 16;");
+
+        HBox hboxBottone = new HBox();
+        Pane spacing=new Pane();
+        spacing.setPrefHeight(10);
+        hboxBottone.setAlignment(Pos.CENTER_RIGHT);
         Button bu1=new Button("Inserisci");
-        rootOperation.getChildren().addAll(l1,t1,l2,t2,l3,t3,l4,t4,l5,t5,l6,t6,bu1);
+        bu1.setStyle("-fx-background-color: #d6ccc2; -fx-background-radius: 16; -fx-textFill: #edede9;");
+        bu1.setPrefSize(96,24);
+        hboxBottone.getChildren().add(bu1);
+
+        rootOperation.getChildren().addAll(l1,t1,l2,t2,l3,t3,l4,t4,l5,t5,l6,t6,spacing,hboxBottone);
         bu1.setOnAction( e ->{
             spettacolo=new Spettacolo(t1.getText(),t2.getText(),t3.getText(),t4.getText(),Double.parseDouble(t5.getText()),Integer.parseInt(t6.getText()));
             rootOperation.getChildren().clear();
